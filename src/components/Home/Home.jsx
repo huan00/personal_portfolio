bdfc7eae977fcf4cbe03b6be542b7fb1ef49fa16
huan00 <96999144+huan00@users.noreply.github.com>
2022-08-05 13:58:48 -0400

    add navbar and style.

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import './styles.css'
+
+const Home = () => {
+  return (
+    <div className="home">
+      <div className="home-title">
+        <div className="home-title1">
+          <h1>SOFTWARE</h1>
+        </div>
+        <div className="home-title2">
+          <h1>ENGINEER</h1>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Home

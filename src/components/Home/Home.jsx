ca5bf36a8cd2c199ab9d818993652022584e46d3
huan00 <96999144+huan00@users.noreply.github.com>
2022-08-09 13:52:33 -0400

    add animation to profile

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,16 +1,28 @@
-import React from 'react'
+import { useState } from 'react'
 import Profile from '../Profile/Profile'
 import WorkStatus from '../WorkStatus/WorkStatus'
 import Project from '../Project/Project'
+import Skill from '../Skill/Skill'
 
 import './styles.css'
+import Contact from '../Contact/Contact'
 
 const Home = () => {
+  const [titleColor, setTitleColor] = useState('white')
+
+  const handleTitleColor = (color) => {
+    setTitleColor(color)
+  }
+
   return (
     <div className="home">
-      <Profile />
-      <WorkStatus />
-      <Project />
+      <div className="home-content">
+        <Profile titleColor={titleColor} />
+        <Skill handleTitleColor={handleTitleColor} />
+        <WorkStatus />
+        <Project />
+      </div>
+      <Contact />
     </div>
   )
 }
